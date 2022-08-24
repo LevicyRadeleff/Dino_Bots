52030b51ffb60a222eb6ea163d02eff4d3e519fd
LevicyRadeleff <levicyradeleff@gmail.com>
2022-08-23 20:57:56 -0700

    updates

--- stacksEditor.py
+++ stacksEditor.py
@@ -84,7 +84,7 @@ elif addBalFlag == 4:
 for item in dinoContent:
     output.write("!adddino <@" + str(userID) + "> " + item + " " + str(userValue) + '\n')
 
-if addBalFlag not 4:
+if addBalFlag is not 4:
 	for item in zippedFile:
 	    if item[1] != 0:
 	        output.write("!adddino <@" + str(userID) + "> " + item[0] + " " + str(item[1]) + '\n')
